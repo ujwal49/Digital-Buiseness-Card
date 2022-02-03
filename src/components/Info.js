752bd8836586cc1d363ca243633dfd1474c1c818
ujwal49 <65085816+ujwal49@users.noreply.github.com>
2022-02-03 19:03:44 +0530

    Add static content

---
+++ src/components/Info.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import profile_pic from "../img/profile_picture.png"
+import "./Info.css"
+import About from "./About"
+import Intrests from './Intrests'
+
+export default function Info(){
+    return(
+        <div>
+            <img src={profile_pic} />
+            <h1 className="name">Laura Smith</h1>
+            <h3 className="developer">Frontend Developer</h3>
+            <h5>laurasmith.website</h5>
+
+            <div>
+                <button>Email</button>
+                <button>Linkedin</button>
+            </div>
+
+            <About />
+            <Intrests />
+        </div>
+    )
+}
---
+++ src/components/Intrests.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Intrests(){
+    return(
+        <div>
+            <h2>Intrests</h2>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}

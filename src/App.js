a210b572258c17400379506bc0307749d1958228
ujwal49 <65085816+ujwal49@users.noreply.github.com>
2022-02-03 19:14:25 +0530

    refactor move About and Intrest to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Info from "./components/Info"
+import About from "./About"
+import Intrests from './Intrests'
 
 export default function App(){
     return(
-        <>
+        <div>
             <Info />
-        </>
+            <About />
+            <Intrests />
+        </div>
     )
 }
